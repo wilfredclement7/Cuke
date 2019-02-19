f9334bc63786e4f2c438129b8b7bbfbe3c43932c
wilfredclement7 <wilfredclement7@hotmail.com>
2019-02-19 19:45:28 +0530

    Update
    Update

---
+++ src/main/java/utilities/RestAssuredExtension.java
@@ -0,0 +1,12 @@
+package utilities;
+
+import io.restassured.builder.RequestSpecBuilder;
+
+public class RestAssuredExtension {
+	
+	public RestAssuredExtension() {
+		
+		RequestSpecBuilder builder = new RequestSpecBuilder();
+	}
+
+}
