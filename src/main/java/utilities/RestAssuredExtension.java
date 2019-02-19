8174cc2b327d8b9854a271e1a5816e2755330d76
wilfredclement7 <wilfredclement7@hotmail.com>
2019-02-19 20:28:36 +0530

    Update
    Update

--- src/main/java/utilities/RestAssuredExtension.java
+++ src/main/java/utilities/RestAssuredExtension.java
@@ -1,12 +1,22 @@
 package utilities;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 public class RestAssuredExtension {
+	
+	public static RequestSpecification Request;
 	
 	public RestAssuredExtension() {
 		
 		RequestSpecBuilder builder = new RequestSpecBuilder();
+		builder.setBaseUri("http://127.0.0.1:3000");
+		builder.setContentType(ContentType.JSON);
+		
+		var request = builder.build();
+		Request = RestAssured.given().spec(request);
 	}
 
 }
